34f05e21483695eb96636617796dc33a8472db9b
Craig Andrews <candrews@integralblue.com>
2016-06-15 09:43:50 -0400

    Rename WebSocketConfig to WebSocketMessageBrokerConfig for clarity; set max websock message size to 16mb

--- web/src/main/java/com/integralblue/commander/web/WebSocketMessageBrokerConfig.java
+++ web/src/main/java/com/integralblue/commander/web/WebSocketMessageBrokerConfig.java
@@ -7,10 +7,14 @@ import org.springframework.messaging.simp.user.UserDestinationResolver;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketMessageBrokerConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	private static final int MAX_MESSAGE_SIZE = 16 * 1024 * 1024;
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,6 +25,20 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 		config.setApplicationDestinationPrefixes("/app");
 
 		config.setUserDestinationPrefix("/user");
+	}
+
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+		super.configureWebSocketTransport(registration);
+		registration.setMessageSizeLimit(MAX_MESSAGE_SIZE); // 1 MB
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(MAX_MESSAGE_SIZE);
+		container.setMaxBinaryMessageBufferSize(MAX_MESSAGE_SIZE);
+		return container;
 	}
 
 	@Override
--- web/src/test/java/com/integralblue/commander/web/CommanderWebInterfaceApplicationTests.java
+++ web/src/test/java/com/integralblue/commander/web/CommanderWebInterfaceApplicationTests.java
@@ -2,6 +2,7 @@ package com.integralblue.commander.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -9,6 +10,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CommanderWebInterfaceApplication.class)
 @WebAppConfiguration
+// Run as an integration test to workaround
+// https://jira.spring.io/browse/SPR-14367
+@IntegrationTest("server.port:0")
 public class CommanderWebInterfaceApplicationTests {
 
 	@Test
