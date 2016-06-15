2936de5799c977ecbe0af06ba4f7225378e33664
Craig Andrews <candrews@integralblue.com>
2016-06-15 12:00:47 -0400

    Rename test class to match package name

--- web/src/test/java/com/integralblue/commander/web/CommanderWebInterfaceApplicationTests.java
+++ web/src/test/java/com/integralblue/commander/web/CommanderWebInterfaceApplicationTests.java
@@ -1,13 +1,10 @@
-package web;
+package com.integralblue.commander.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.integralblue.commander.web.CommanderWebInterfaceApplication;
-
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CommanderWebInterfaceApplication.class)
