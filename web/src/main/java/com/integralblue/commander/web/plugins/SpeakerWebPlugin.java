4ca64745071023d619625d7a74c0e7abbd9345c1
Craig Andrews <candrews@integralblue.com>
2016-06-15 09:48:39 -0400

    The WAV format requires the audio length to be known ahead of time, so when writing that format, transcode first to get the length

--- web/src/main/java/com/integralblue/commander/web/plugins/SpeakerWebPlugin.java
+++ web/src/main/java/com/integralblue/commander/web/plugins/SpeakerWebPlugin.java
@@ -1,9 +1,14 @@
 package com.integralblue.commander.web.plugins;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.CompletionStage;
 
 import javax.sound.sampled.AudioFileFormat.Type;
+import javax.sound.sampled.AudioFormat.Encoding;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 
@@ -15,6 +20,7 @@ import lombok.NonNull;
 import lombok.SneakyThrows;
 
 public class SpeakerWebPlugin extends AbstractWebPlugin implements Speaker {
+	private static final int BUFFER_SIZE = 4096;
 
 	@Override
 	@SneakyThrows
@@ -22,7 +28,17 @@ public class SpeakerWebPlugin extends AbstractWebPlugin implements Speaker {
 		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		// TODO get the supported audio types from the client and only send one
 		// of those types
-		AudioSystem.write(audioData.getAudioInputStream(), Type.WAVE, baos);
+		AudioInputStream ais = audioData.getAudioInputStream();
+		
+		// in order to write a WAV file, have to know how long the file is
+		if(ais.getFrameLength()==AudioSystem.NOT_SPECIFIED){
+			ais = AudioSystem.getAudioInputStream(Encoding.PCM_SIGNED, ais);
+			ByteArrayOutputStream baos2 = new ByteArrayOutputStream();
+			copy(ais,baos2);
+			ais = new AudioInputStream(new ByteArrayInputStream(baos2.toByteArray()), ais.getFormat(), baos2.size() / ais.getFormat().getFrameSize());
+		}
+		
+		AudioSystem.write(ais, Type.WAVE, baos);
 		return sendRequestToClient(
 				SpeakerRequestToClient.builder().mimeType("audio/wav").audio(baos.toByteArray()).build())
 						.thenRun(() -> {
@@ -39,4 +55,24 @@ public class SpeakerWebPlugin extends AbstractWebPlugin implements Speaker {
 		return playAsync(AudioData.fromAudioInputStream(audioInputStream));
 	}
 
+
+	/**
+	 * Copy the contents of the given InputStream to the given OutputStream.
+	 * Leaves both streams open when done.
+	 *
+	 * @param in
+	 *            the InputStream to copy from
+	 * @param out
+	 *            the OutputStream to copy to
+	 * @throws IOException
+	 *             in case of I/O errors
+	 */
+	private static void copy(@NonNull InputStream in, @NonNull OutputStream out) throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE];
+		int bytesRead = -1;
+		while ((bytesRead = in.read(buffer)) != -1) {
+			out.write(buffer, 0, bytesRead);
+		}
+		out.flush();
+	}
 }
