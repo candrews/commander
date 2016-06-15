fc9757061578ad36eac5837285a15a4bbdd023b4
Craig Andrews <candrews@integralblue.com>
2016-06-15 09:46:16 -0400

    Log the reason when a disconnect occurs

--- web/src/main/java/com/integralblue/commander/web/service/BrainService.java
+++ web/src/main/java/com/integralblue/commander/web/service/BrainService.java
@@ -32,8 +32,10 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class BrainService implements ApplicationContextAware {
 
 	@Component
@@ -168,6 +170,7 @@ public class BrainService implements ApplicationContextAware {
 	}
 
 	private void onApplicationEvent(SessionDisconnectEvent event) {
+		log.info("Disconnected. Close status: {}", event.getCloseStatus());
 		final String sessionId = event.getSessionId();
 		endSession(sessionId);
 	}
